2e5ad4bb0e51200bd50a74137fccf5cb1bdd05bd
root <root@precise32.(none)>
2014-07-28 14:22:30 +0000

    initial logstash commit

---
+++ logstash/attributes/default.rb
@@ -0,0 +1,4 @@
+# set default attributes
+
+node.default['elasticsearch']['version'] = "1.3.0"
+node.default['logstash']['version'] = "1.4.2"
---
+++ logstash/recipes/default.rb
@@ -0,0 +1,50 @@
+
+# create directories
+
+directory '/opt/logstash-#{node[:logstash][:version]}' do
+  user 'logstash'
+  group 'logstash'
+  mode '0755'
+end
+
+directory '/etc/logstash' do
+  user 'logstash'
+  group 'logstash'
+  mode '0755'
+end
+
+directory '/var/log/logstash' do
+  user 'logstash'
+  group 'logstash'
+  mode '0755'
+end
+
+# install packages
+
+bash 'install elasticsearch' do
+  not_if "/opt/logstash/elasticsearch --version | grep -q '#{node[:elasticsearch][:version]}'" #maintain idempotency if package already exists
+  user "root"
+  cwd "/opt/logstash/"
+  code <<-EOH
+    wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-#{node[:elasticsearch][:version]}.deb
+    dpkg -i elasticsearch-#{node[:elasticsearch][:version]}.deb
+    mv elasticsearch-#{node[:elasticsearch][:version]}.deb /var/cache/apt/archives
+  EOH
+end
+
+bash 'install logstash' do
+  not_if {File.exists?("/opt/logstash-#{node[:logstash][:version]}/bin/logstash.bat")} #maintain idempotency if files already exists
+  user "root"
+  cwd "/opt/"
+  code <<-EOH
+    wget https://download.elasticsearch.org/logstash/logstash/logstash-#{node[:logstash][:version]}.tar.gz
+    tar -zxf logstash-#{node[:logstash][:version]}.tar.gz
+    mv logstash-#{node[:logstash][:version]}.tar.gz /tmp/
+  EOH
+end
+
+package 'redis-server' do
+  action :install
+end
+
+
